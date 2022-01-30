9741fc1ed57f6cbf04701d3291ab35eda25afdef
Prabhav Bhatnagar <prabhav2b@gmail.com>
2022-01-30 13:52:29 +0200

    footsteps

---
+++ Assets/Scripts/Audio/CharacterFoley.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(AudioSource))]
+public class CharacterFoley : MonoBehaviour
+{
+    [SerializeField] private List<AudioClip> _footStepAudioClips;
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayRandomFootstep()
+    {
+        var audioToBePlayed = _footStepAudioClips[Random.Range(0, _footStepAudioClips.Count - 1)];
+        _audioSource.PlayOneShot(audioToBePlayed);
+    }
+}
